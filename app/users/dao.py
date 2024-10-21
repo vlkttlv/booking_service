96bb27b7f7bfeaba4226004140b0ebb49e5a2cda
Виолетта Брюханова <vvb63@tru.ru>
2024-10-21 20:47:51 +0700

    commit

--- app/users/dao.py
+++
@@ -1,7 +0,0 @@
-from app.dao.base import BaseDAO
-from app.users.models import Users
-
-
-class UsersDAO(BaseDAO):
-    """Класс для работы с БД. Наследуется от базового класса"""
-    model = Users
--- app/users/schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-
-class SUserAuth(BaseModel):  # схемы нужны для валидации данных
-    """Схема для аутенфикации пользователя"""
-    email: EmailStr
-    password: str

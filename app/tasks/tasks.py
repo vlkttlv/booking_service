156ab43902d3f9c852a8b219d5cfbf9b54f45c81
Виолетта Брюханова <vvb63@tru.ru>
2024-10-28 15:39:37 +0700

    добавлен метод для отправки письма об оплате + логер + исправлен получатель писем

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -6,7 +6,8 @@ from pydantic import EmailStr
 
 from app.config import settings
 from app.tasks.celery import celery
-from app.tasks.email_templates import create_booking_confirmation_template
+from app.tasks.email_templates import create_booking_confirmation_template, create_pay_confirmation_template
+from app.logger import logger
 
 
 @celery.task
@@ -29,9 +30,22 @@ def send_booking_confirmation_email(
     booking: dict,
     email_to: EmailStr,
 ):
-    email_to = settings.SMTP_USER
     msg_content = create_booking_confirmation_template(booking, email_to)
 
     with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
         server.login(settings.SMTP_USER, settings.SMTP_PASS)
         server.send_message(msg_content)
+    logger.info(f"Successfully send email message to {email_to}")
+
+
+@celery.task
+def send_pay_confirmation_email(
+    booking: dict,
+    email_to: EmailStr,
+):
+    msg_content = create_pay_confirmation_template(booking, email_to)
+
+    with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT) as server:
+        server.login(settings.SMTP_USER, settings.SMTP_PASS)
+        server.send_message(msg_content)
+    logger.info(f"Successfully send email message to {email_to}")

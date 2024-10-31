9a5c5822216bbd149660df116267f0779a127e86
Виолетта Брюханова <vvb63@tru.ru>
2024-10-31 22:20:23 +0700

    удалила ненужную таску

--- app/tasks/tasks.py
+++ app/tasks/tasks.py
@@ -10,20 +10,6 @@ from app.tasks.email_templates import create_booking_confirmation_template, crea
 from app.logger import logger
 
 
-@celery.task
-def process_pic(
-    path: str,
-):
-    im_path = Path(path)
-    im = Image.open(im_path)
-    for width, height in [
-        (1000, 500),
-        (200, 100)
-    ]:
-        resized_img = im.resize(size=(width, height))
-        resized_img.save(
-            f"app/static/images/resized_{width}_{height}_{im_path.name}")
-
 
 @celery.task
 def send_booking_confirmation_email(

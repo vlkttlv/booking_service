6c8613b29e3c5ccbef2bdd7edee53a9a4cc96b08
Виолетта Брюханова <vvb63@tru.ru>
2024-11-01 20:18:54 +0700

    tests (1)

--- app/tests/integration_tests/test_booking/test_booking_api.py
+++ app/tests/integration_tests/test_booking/test_booking_api.py
@@ -4,35 +4,30 @@ import pytest
 
 
 @pytest.mark.parametrize("room_id,date_from,date_to,booked_rooms,status_code", [
-    (10, "2025-08-10", "2025-09-01", 3, 200),
-    (10, "2025-08-10", "2025-09-01", 4, 200),
-    (10, "2025-08-10", "2025-09-01", 5, 200),
-    (10, "2025-08-10", "2025-09-01", 6, 200),
-    (10, "2025-08-10", "2025-09-01", 7, 200),
-    (10, "2025-08-10", "2025-09-01", 8, 200),
-    (10, "2025-08-10", "2025-09-01", 9, 200),
-    (10, "2025-08-10", "2025-09-01", 9, 409),
+    (1, "2025-08-10", "2025-09-01", 3, 200),
+    (1, "2025-08-10", "2025-09-01", 4, 200),
+    (1, "2025-08-10", "2025-09-01", 5, 200),
+    (1, "2025-08-10", "2025-09-01", 5, 409),
+    # 2 брони уже есть, еще 3 добавятся, 5я уже нет (всего 5 комнат)
 
 ])
 async def test_add_and_get_booking(auth_ac: AsyncClient, room_id, date_from, date_to, status_code, booked_rooms):
-    response = await auth_ac.post("/bookings", params={"room_id": room_id,
-                                                       "date_from": datetime.strptime(date_from, "%Y-%m-%d"),
-                                                       "date_to": datetime.strptime(date_to, "%Y-%m-%d")})
+    response = await auth_ac.post("/v1/bookings", params={"room_id": room_id,
+                                                          "date_from": datetime.strptime(date_from, "%Y-%m-%d"),
+                                                          "date_to": datetime.strptime(date_to, "%Y-%m-%d")})
 
     assert response.status_code == status_code
-    response = await auth_ac.get('/bookings')
+    response = await auth_ac.get('/v1/bookings')
     assert len(response.json()) == booked_rooms
 
 
 async def test_get_and_delete_bookings(auth_ac: AsyncClient):
-    # отправляем GET - запрос на получение отелей
-    response = await auth_ac.get('/bookings')
-    # получаем все брони в виде списка
+    response = await auth_ac.get('/v1/bookings')
+    # получаем все айди броней в виде списка
     all_bookings = [booking['id'] for booking in response.json()]
     for booking_id in all_bookings:  # проходимся по каждому бронированию и удаляем его
         response = await auth_ac.delete(
-            f"/bookings/{booking_id}",
+            f"/v1/bookings/{booking_id}",
         )
-    # отправляем GET - запрос на получение отелей
-    response = await auth_ac.get("/bookings")
-    assert len(response.json()) == 0  # проверка, что список броней пуст
+    response = await auth_ac.get("/v1/bookings")
+    assert len(response.json()) == 0  # проверка, что у 1 юзера 0 броней
--- app/tests/integration_tests/test_users/test_api.py
+++ app/tests/integration_tests/test_users/test_api.py
@@ -4,14 +4,14 @@ import pytest
 
 
 @pytest.mark.parametrize("email,password,status_code", [
-    ("sobaka@pes.com", "sobaka2000", 200),
-    ("sobaka@pes.com", "qwerty", 409),
-    ("pes@pes.com", "qwerty", 200),
+    ("example@gmail.com", "sobaka2000", 201),
+    ("example@gmail.com", "qwerty", 409),
+    ("mock@mock.com", "qwerty", 201),
     ("ne_email", "qwerty", 422)])
 async def test_register_user(ac: AsyncClient, email,
                              password, status_code):
     """Тест, проверяющий регистрацию пользователя"""
-    response = await ac.post("/auth/register", json={
+    response = await ac.post("/v1/auth/register", json={
         "email": email,
         "password": password,
     })
@@ -20,12 +20,12 @@ async def test_register_user(ac: AsyncClient, email,
 
 @pytest.mark.parametrize("email,password,status_code", [
     ("test@test.com", "test", 200),
-    ("test@test.com", "fake_test", 401),
-    ("fake@people.com", "pass", 401)
+    ("test@test.com", "wrong_test", 401),
+    ("fake@test.com", "password", 401)
 ])
 async def test_login_user(ac: AsyncClient, email, password, status_code):
     """Тест, проверяющий вход пользователя в систему"""
-    response = await ac.post("/auth/login", json={
+    response = await ac.post("/v1/auth/login", json={
         "email": email,
         "password": password,
     })
